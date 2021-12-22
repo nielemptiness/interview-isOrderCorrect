4f7f2038ab0b90a488cc890c16aa5e0919c9e563
Proniaiev Maksym <m.pronyayev@beter.co>
2021-12-22 23:14:39 +0200

    implement

---
+++ IsOrderCorrect/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "(";
+            var result = text.ToCharArray();
+            var openLine = '(';
+            var endLine = ')';
+
+            int occurCount = 0;
+            foreach (var t in text)
+            {
+                if (t == openLine)
+                {
+                    occurCount++;
+                    continue;
+                }
+
+                if (t == endLine)
+                {
+                    occurCount--;
+                }
+
+                if (occurCount < 0) throw new ArgumentException("Incorrect order!");
+            }
+
+            if (occurCount == 0)
+            {
+                Console.WriteLine("Success!");
+                return;
+            }
+
+            throw new ArgumentException("Incorrect order!");
+        }
+    }
+}
